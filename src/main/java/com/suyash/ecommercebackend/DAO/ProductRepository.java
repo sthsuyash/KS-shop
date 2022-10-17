cee491693fc05c39e65ce601970a3f25123ab270
Suyash Shrestha <sthasuyash11@gmail.com>
2022-10-17 19:52:56 +0545

    add Cross Origin

--- src/main/java/com/suyash/ecommercebackend/DAO/ProductRepository.java
+++ src/main/java/com/suyash/ecommercebackend/DAO/ProductRepository.java
@@ -1,9 +1,9 @@
-package com.suyash.ecommercebackend.dao;
+package com.suyash.ecommercebackend.DAO;
 
 import com.suyash.ecommercebackend.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@Repository
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
