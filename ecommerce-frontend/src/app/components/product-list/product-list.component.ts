8045ebca7f22780d11e76fa6062e76f9b76bb9af
Suyash Shrestha <sthasuyash11@gmail.com>
2022-10-18 18:16:20 +0545

    add HTML table to show products

--- ecommerce-frontend/src/app/components/product-list/product-list.component.ts
+++ ecommerce-frontend/src/app/components/product-list/product-list.component.ts
@@ -4,7 +4,8 @@ import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
+  // templateUrl: './product-list.component.html',
+  templateUrl: './product-list.component-table-format.html',
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
